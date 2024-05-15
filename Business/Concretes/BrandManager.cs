844ab74332c47e71c04d46008223acfd3142797d
baver.gunay <baver.gunay@TSBP_TTO01>
2024-05-15 10:52:26 +0300

    Add project files.

---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Request;
+using Business.Dtos.Responses;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes;
+
+public class BrandManager : IBrandService
+{
+    IBrandDal _brandDal;
+
+    public BrandManager(IBrandDal brandDal)
+    {
+         _brandDal = brandDal;
+    }
+
+    public CreatedBrandResponse Add(CreatedBrandRequest CreatedBrandRequest)
+    {
+        //business rules
+
+        Brand brand =new Brand();
+        brand.Name= CreatedBrandRequest.Name;   
+        brand.CreatedDate=DateTime.Now;
+
+        _brandDal.Add(brand);
+        CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+        createdBrandResponse.Name = brand.Name;
+        createdBrandResponse.Id = 4;
+        createdBrandResponse.CreateDate= brand.CreatedDate;
+        return createdBrandResponse;    
+    }
+
+    public List<GetAllBrandResponse> GetAll()
+    {
+       List<Brand> brands=_brandDal.GetAll();
+
+       List<GetAllBrandResponse> getAllBrandResponse = new List<GetAllBrandResponse>();
+
+      foreach (var brand in brands)
+        {
+            GetAllBrandResponse getAllBrandResponses = new GetAllBrandResponse();
+            getAllBrandResponses.Name = brand.Name;
+            getAllBrandResponses.Id = brand.Id;
+            getAllBrandResponses.CreateDate = brand.CreatedDate;
+
+            getAllBrandResponse.Add(getAllBrandResponses);
+        }
+      return getAllBrandResponse;
+    }
+    
+}
---
+++ WepApi/Controllers/BrandsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Business.Dtos.Responses;
+using Business.Dtos.Request;
+using Business.Abstracts;
+
+namespace WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpPost]
+       public IActionResult Add(CreatedBrandRequest CreatedBrandRequest)
+        {
+         CreatedBrandResponse createdBrandResponse=
+                _brandService.Add(CreatedBrandRequest);
+            return Ok(createdBrandResponse);
+        }
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok(_brandService.GetAll());
+        }
+    }
+}
